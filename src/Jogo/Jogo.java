206265791f7b39fc3b4dee52b791ea94e1bf2a53
Roberto Dias <rrrobertodias@gmail.com>
2020-05-28 22:14:37 -0300

    Commit: Final

--- src/Jogo/Jogo.java
+++ src/Jogo/Jogo.java
@@ -25,56 +25,86 @@ public class Jogo {
 		System.out.println("* Contagem Especial: " + especiais);
 		System.out.println("=======================");
 	}
-	static void Combate() {
+	static int Combate() {
 		int hpUsuario = 150;
-		int hpCpu = 11;
+		int hpCpu;
 		int especiais = 5;
 		int ataque;
-		while(hpUsuario > 0 && hpCpu > 0) {
-			HP(hpUsuario, hpCpu, especiais);
-			ataque = AtaqueUsuario();
-			switch(ataque) {
-			    case 1:
-			    	System.out.println("Usuario Atacou com um Soco.");
-			    	hpCpu -= 7;
-			    	break;
-			    case 2:
-			    	System.out.println("Usuario Atacou com um Especial.");
-			    	hpCpu -= 20;
-			    	especiais--;
-			    	break;
-			    default:
-			    	System.out.println("Opção Invalida.");
-			    	break;
-			}
-			if(hpCpu > 0) {
-				ataque = AtaqueCpu();
-				switch(ataque) {
-				case 1:
-					System.out.println("Oponente Atacou com um Soco.");
-					hpUsuario -= 2;
-					break;
-				case 2:
-					System.out.println("Oponente Atacou com um Chute.");
-					hpUsuario -= 3;
-					break;
-				case 3:
-					System.out.println("Oponente Atacou com um Especial");
-					hpUsuario -= 4;
-				    break;
-				}
-				
-			}else {
-				System.out.println("Inimigo Derrotado");
-			}
-		}
+		int i = 1;
 		
+		while(hpUsuario > 0) {
+			hpCpu = 11 + i;
+			
+			System.out.println("=-=-=-=-=-=-=-=-=-=");
+			System.out.println("    INIMIGO " + i);
+			System.out.println("=-=-=-=-=-=-=-=-=-=\n");
+			
+			while(hpUsuario > 0 && hpCpu > 0) {
+				HP(hpUsuario, hpCpu, especiais);
+				ataque = AtaqueUsuario();
+				switch(ataque) {
+				    case 1:
+				    	System.out.println("Usuario Atacou com um Soco.");
+				    	hpCpu -= 7;
+				    	break;
+				    case 2:
+				    	System.out.println("Usuario Atacou com um Especial.");
+				    	hpCpu -= 20;
+				    	especiais--;
+				    	break;
+				    default:
+				    	System.out.println("Opção Invalida.");
+				    	break;
+				}
+				if(hpCpu > 0) {
+					ataque = AtaqueCpu();
+					switch(ataque) {
+					case 1:
+						System.out.println("Oponente Atacou com um Soco.");
+						hpUsuario -= 2 + (int) (i / 10);
+						break;
+					case 2:
+						System.out.println("Oponente Atacou com um Chute.");
+						hpUsuario -= 3 + (int)(i / 10);
+						break;
+					case 3:
+						System.out.println("Oponente Atacou com um Especial");
+						hpUsuario -= 4 + (int) (i / 20);
+					    break;
+					}
+					
+				}else {
+					System.out.println("Inimigo Derrotado");
+				}
+			}
+			if(hpUsuario > 0) {
+				hpUsuario += 5;
+				if(hpUsuario > 150) {
+					hpUsuario = 150;
+				}
+				if(i % 10 == 0) {
+					especiais++;
+					if(especiais > 5) {
+						especiais = 5;
+					}
+				}
+			}
+			
+			i++;
+		}
+		return i;
 	}
 	public static void main(String [] args) {
 		int cont = 1;
+		int record = 0;
 		while(cont == 1) {
 			
-			Combate();
+			int inimigos = Combate();
+			if(inimigos > record) {
+				record = inimigos;
+			}
+			System.out.println("Record Atual: " + record);
+			
 			
 			System.out.println("Fim do Jogo. Deseja Continuar?");
 			System.out.println("(1) Sim");
